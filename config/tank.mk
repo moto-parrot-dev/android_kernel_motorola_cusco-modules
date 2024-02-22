c3e88f667b692ba60ffff7f947550a0cb780144d
wangxiaofeng <wangxf32@motorola.com>
2024-02-22 17:22:32 +0800

    tank: Add retries for sensor id matching
    details:
    Add retries in the sensor id matching during kernel camera
    probe to handle any transient failures
    
    Change-Id: Ia78f2c1961e0b383450fc3608081683189a3ab0b
    Signed-off-by: wangxiaofeng <wangxf32@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2886659
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Simon C <chenzm8@motorola.com>
    Reviewed-by: wangdp11 <wangdp11@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/tank.mk
+++ config/tank.mk
@@ -5,9 +5,11 @@ ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 CONFIG_CCI_DEBUG_INTF := y
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 endif
+CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
 # Flags to pass into C preprocessor
 CONFIG_MOT_OIS_DW9784_DRIVER := y
 CONFIG_MOT_OIS_EARLY_UPGRADE_FW := y
 ccflags-y += -DCONFIG_MOT_OIS_DW9784_DRIVER=1
 ccflags-y += -DCONFIG_MOT_OIS_EARLY_UPGRADE_FW=1
+ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
